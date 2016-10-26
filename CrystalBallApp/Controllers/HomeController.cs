bace39684f95527cb3ec84ef92d9738eb6c22234
csirirathasuk14 <csirirathasuk14@gmail.com>
2016-10-26 09:48:53 -0400

    first commit

---
+++ CrystalBallApp/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using CrystalBallApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrystalBallApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult ShakeBall()
+        {
+            CrystalBall ball = new CrystalBall();
+            string reply = ball.getResponse();
+            return Json(reply, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ CrystalBallApp/Models/CrystalBall.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrystalBallApp.Models
+{
+    public class CrystalBall
+    {
+        public string getResponse()
+        {
+            string[] responses = { "You are loved", "It's best to serve soup warm", "gg my dude" };
+            Random randomNum = new Random();
+            int num = randomNum.Next(0, responses.Length);
+            return responses[num];
+        }
+    }
+}
